f3560b36fbd7fc087be3af58790a02d973e8d31e
yifita <yifanwang2010@gmail.com>
2018-12-20 21:55:59 +0000

    starting main.py

---
+++ main.py
@@ -0,0 +1,98 @@
+import torch
+import argparse
+import os
+import time
+import numpy as np
+
+from net import Net
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--phase', default='test',
+                    help='train or test [default: train]')
+parser.add_argument('--gpu', default='0', help='GPU to use [default: GPU 0]')
+parser.add_argument('--id', help="experiment name, prepended to log_dir")
+parser.add_argument('--log_dir', default='../model',
+                    help='Log dir [default: log]')
+parser.add_argument('--model', default='model_microscope', help='model name')
+parser.add_argument('--root_dir', default='../',
+                    help='project root, data and h5_data diretories')
+parser.add_argument('--result_dir', help='result directory')
+parser.add_argument('--restore', help='model to restore from')
+parser.add_argument('--num_point', type=int,
+                    help='Point Number [1024/2048] [default: 1024]')
+parser.add_argument('--num_shape_point', type=int,
+                    help="Number of points per shape")
+parser.add_argument('--up_ratio', type=int, default=4,
+                    help='Upsampling Ratio [default: 2]')
+parser.add_argument('--max_epoch', type=int, default=160,
+                    help='Epoch to run [default: 500]')
+parser.add_argument('--batch_size', type=int, default=28,
+                    help='Batch Size during training [default: 32]')
+parser.add_argument('--h5_data', help='h5 file for training')
+parser.add_argument('--record_data', help='record file for training')
+parser.add_argument(
+    '--test_data', default='data/test_data/sketchfab_poisson/poisson_5000/*.xyz', help='h5 file for training')
+parser.add_argument('--learning_rate', type=float, default=0.0005)
+parser.add_argument('--restore_epoch', type=int)
+parser.add_argument('--stage_steps', type=int, default=15000,
+                    help="number of updates per curriculums stage")
+parser.add_argument('--step_ratio', type=int, default=2,
+                    help="upscale ratio per step")
+parser.add_argument('--patch_num_ratio', type=float, default=3)
+parser.add_argument('--jitter', action="store_true",
+                    help="jitter augmentation")
+parser.add_argument('--jitter_sigma', type=float,
+                    default=0.0025, help="jitter augmentation")
+parser.add_argument('--jitter_max', type=float,
+                    default=0.005, help="jitter augmentation")
+parser.add_argument('--drop_out', type=float, default=1.0,
+                    help="drop_out ratio. default 1.0 (no drop out) ")
+parser.add_argument('--knn', type=int, default=32,
+                    help="neighbood size for edge conv")
+parser.add_argument('--dense_n', type=int, default=3,
+                    help="number of dense layers")
+parser.add_argument('--block_n', type=int, default=3,
+                    help="number of dense blocks")
+parser.add_argument('--fm_knn', type=int, default=5,
+                    help="number of neighboring points for feature matching")
+
+parser.add_argument('--cd_threshold', default=2.0,
+                    type=float, help="threshold for cd")
+parser.add_argument('--fidelity_weight', default=50.0,
+                    type=float, help="chamfer loss weight")
+
+FLAGS = parser.parse_args()
+PHASE = FLAGS.phase
+GPU_INDEX = FLAGS.gpu
+ROOT_DIR = FLAGS.root_dir
+MODEL_DIR = os.path.join(FLAGS.log_dir, FLAGS.id)
+ASSIGN_MODEL_PATH = FLAGS.restore
+
+NUM_POINT = FLAGS.num_point or int(FLAGS.num_shape_point * FLAGS.drop_out)
+
+BATCH_SIZE = FLAGS.batch_size
+MAX_EPOCH = FLAGS.max_epoch
+BASE_LEARNING_RATE = FLAGS.learning_rate
+JITTER = FLAGS.jitter
+STAGE_STEPS = FLAGS.stage_steps
+
+STEP_RATIO = FLAGS.step_ratio
+RESTORE_EPOCH = FLAGS.restore_epoch
+NUM_SHAPE_POINT = FLAGS.num_shape_point
+FM_KNN = FLAGS.fm_knn
+KNN = FLAGS.knn
+GROWTH_RATE = FLAGS.growth_rate
+DENSE_N = FLAGS.dense_n
+
+UP_RATIO = FLAGS.up_ratio
+TRAIN_H5 = FLAGS.h5_data
+TRAIN_RECORD = FLAGS.record_data
+
+TEST_DATA = FLAGS.test_data
+PATCH_NUM_RATIO = FLAGS.patch_num_ratio
+
+
+net = Net(max_up_ratio=UP_RATIO, step_ratio=STEP_RATIO,
+          knn=KNN, growth_rate=GROWTH_RATE, dense_n=DENSE_N, fm_knn=FM_KNN)
+
+print(net)
